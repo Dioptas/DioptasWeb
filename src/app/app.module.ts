5d9f5ab7f34053bc3efe31f5ef83d777348dac1c
Clemens Prescher <clemens.prescher@gmail.com>
2021-01-02 12:01:02 +0100

    First Setup with ng-materials and plotly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlotlyExampleComponent } from './plotly-example/plotly-example.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+
+import { MaterialsModule } from './shared/materials.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlotlyExampleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PlotlyModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
